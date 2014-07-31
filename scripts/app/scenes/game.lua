e6c9fdfdb910acb4b85c1be031239e9642cf2e3c
xuyao18 <xuyao18@gmail.com>
2014-07-31 14:32:52 +0800

    【2014-7-31 14:30:54】 init draft add a start menu lua function is wired , pay attention to "." and ":"

---
+++ scripts/app/scenes/game.lua
@@ -0,0 +1,54 @@
+local game = class('game', function()
+	return display.newScene("game")
+end)
+
+function game:ctor()
+	items = {}
+	table.insert(items, {name="start", cb=self.start})
+	local m = self:createMenuScene(items)
+	self:addChild(menu)
+end
+
+function game:startup()
+	CCFileUtils:sharedFileUtils():addSearchPath("res/")
+	game.enterMenuScene()
+
+end
+
+function game:ententerMenuScene()
+	display.replaceScene(require("scenes.MainScene").new(), "fade", 0.6, display.COLOR_WHITE)
+end
+
+function game:createMenuScene(items)
+	local labels = {}
+	for idx, item in ipairs(items) do
+		local label = ui.newTTFLabelMenuItem({
+			text = item.name, 
+			listener = function ()item.cb() end
+		})
+		labels[#labels + 1] = label
+	end
+	menu = ui.newMenu(labels)
+	menu:alignItemsVertically()
+	menu:setPosition(display.cx, display.cy)
+	--self:addChild(menu)
+	return menu
+end
+
+function game:start( ... )
+	print("start ")
+end
+
+function  game:exit()
+	os.exit()
+end
+
+function  game:load()
+	
+end
+
+function game:about()
+	
+end
+
+return game
