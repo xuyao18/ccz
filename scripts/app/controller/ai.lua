a6e0e1d3208a07e673506d1905e29254c9a105ad
xuyao18 <xuyao18@gmail.com>
2014-09-03 18:14:20 +0800

    【2014-9-3 18:14:12】

--- scripts/app/controller/ai.lua
+++ scripts/app/controller/ai.lua
@@ -2,7 +2,7 @@ AI = {}
 AI.__index = AI
 
 local ai_type = {paattack = 1 , poattack = 2, defence = 3, ceattach = 4, tposition = 5, follow = 6, rpostion = 7}
---被动攻击, 主动攻击, 防守，攻击特定目标， 到达某地， 跟随， 逃跑至某地。
+
 local physic_worth = {kill = 78, over10 = 14 , tgtless10 = 14, middle = 8, bad = -4, atklink = {10,4}, confuse = 8, tgtname = 4, tgtfirst = 8}
 local skill_worth = {}
 local postion_worth = {noinrange = 1, cure = 8, imcure = 58, tgtatk = 10, tgtgot = 9 , notgt = -1}
@@ -17,8 +17,20 @@ end
 
 function AI:getAction( unit, handler, x, y)
 	-- after move , what to do next. 	
+	-- if paattack 
 end
 
 function AI:getWorth(starter, action, target)
 	-- get action worth
+end
+
+function AI:aiRun(unit, handler)
+	--1, get move area
+	--pre2, get can phisical&magic area , and targets ,in list or dir
+	--2, calc worth of every area , physic and magic 
+	--3, get the highest worth postion 
+	--4, get route to the postion got .
+	--5, move to target , one tile by on tile 
+	--6, after got target , do action 
+	--7, done action return result .
 end
